21f66e1a2b0aa1f114eb46977825618297c32d10
Bryan <bryanmytko@gmail.com>
2015-06-13 02:57:59 -0400

    Add syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def format_output(raw_content)
+    array = raw_content.split(delimiter)
+    parsed_content = array.each_with_index.map do |chunk, index|
+      (index.odd?) ? format_code(chunk) : chunk
+    end
+    parsed_content.join
+  end
+
+  private
+
+  def delimiter
+    /\[code\]|\[\/code\]/
+  end
+
+  def format_code(string)
+    CodeRay.scan(string.strip, :ruby).div(:line_numbers => :table)
+  end
 end

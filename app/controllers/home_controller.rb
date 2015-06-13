faf305f5e361a141391bf2d42a5365ddb0313ec1
Bryan <bryanmytko@gmail.com>
2015-06-13 02:57:26 -0400

    Remove old calls to admin authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-  skip_before_filter :authenticate_admin!, only: [:index]
-
   def index
     @post = Post.all.limit(10)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  skip_before_filter :authenticate_admin!, only: [:show]
-
   def show
     @post = Post.find(params[:id])
   end

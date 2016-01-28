e8f1f70566f47a48b6e7bf2dee60cd4c8ddcffa2
Bryan Mytko <bryanmytko@gmail.com>
2016-01-28 12:37:08 -0500

    Add edit button and redcarpetize helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,39 @@
 module ApplicationHelper
+  SITE_TITLE = 'www.bryanmytko.com'
+
   def format_output(raw_content)
     array = raw_content.split(delimiter)
     parsed_content = array.each_with_index.map do |chunk, index|
       if index.odd?
         format_code chunk
       else
-        sanitize chunk
+        chunk
       end
     end
+
     parsed_content.join
   end
 
   def page_title
     if @post
-      "#{@post.title} | bryanmytko.com"
+      "#{@post.title} | #{SITE_TITLE}"
     elsif @title
-      "#{@title} | bryanmytko.com"
+      "#{@title} | #{SITE_TITLE}"
     else
-      "bryanmytko.com"
+      SITE_TITLE
     end
+  end
+
+  def admin_edit_button
+    if session["current_admin_user_id"]
+      link_to "[edit]", [:edit, :admin,  @post]
+    end
+  end
+
+  def redcarpetize(content)
+    renderer = Redcarpet::Render::HTML
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    markdown.render(content)
   end
 
   private
@@ -28,6 +43,7 @@ module ApplicationHelper
   end
 
   def format_code(string)
+    string = strip_tags string
     CodeRay.scan(string.strip, :ruby).div(:line_numbers => :table)
   end
 end

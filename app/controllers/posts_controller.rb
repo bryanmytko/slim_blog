9bef68c314c90cd979a901bcd6e9d7f1ebf599ee
Bryan Mytko <bryanmytko@gmail.com>
2016-01-28 12:36:44 -0500

    Move redcarpet responsiblity to a helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,5 @@
 class PostsController < ApplicationController
   def show
-    if @post = Post.find_by(slug: params[:id])
-      renderer = Redcarpet::Render::HTML
-      markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-      @post.content = markdown.render(@post.content)
-    else
-      render_not_found
-    end
+    render_not_found unless @post = Post.find_by(slug: params[:id])
   end
 end

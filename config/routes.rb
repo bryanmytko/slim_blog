e222abc9057ce368facf79d98ff27f158d7fc7f2
Bryan Mytko <bryanmytko@gmail.com>
2016-01-28 12:38:09 -0500

    Update namespaced route syntax so helpers work

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,11 @@ Rails.application.routes.draw do
   resources :visuals, only: :index
   resources :about, only: :index
 
-  scope module: :admin do
-    scope :admin do
-      get '', to: 'admin#index', as: '/'
-      post '/login', to: 'admin#login', as: :admin_login_path
-      get '/logout', to: 'admin#logout', as: :admin_logout_path
-      resources :posts
-    end
+  namespace :admin do
+    get '', to: 'admin#index', as: '/'
+    post '/login', to: 'admin#login', as: :admin_login_path
+    get '/logout', to: 'admin#logout', as: :admin_logout_path
+    resources :posts
   end
 
   get "sitemaps/sitemap.xml.gz", to: "sitemaps#show"
